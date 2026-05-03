8db661da216c6a3c32a79365c2b07ec2f1b93e33
AnkushSatpute2005 <ankushsatpute2005@gmail.com>
2026-05-03 21:15:12 +0530

    Commited on 3 May 2026 21:15 pm

--- backend/controllers/userContoler.js
+++ backend/controllers/userContoler.js
@@ -68,5 +68,95 @@ const createToken = (userId)=>{
 
         })
     }
+    try {
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:"User not found."
+            });
+        }
+
+        const match = await bcrypt.compare(password,user.password)
+        if(!match){
+            return res.status(401).json({
+                success:false,
+                message:"Invalid Password"
+            })
+        }
+
+        const token = createToken(user._id);
+        res.json({
+            success:true,
+            token,
+            user:{
+                id:user._id,
+                name:user.name,
+                emai:user.email;
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Server Error."
+        });
+    }
     
+ }
+
+ // Get User
+
+ export async function getCurrentUser(req,res) {
+    try {
+        const user = await User.findById(req.user.id).select("name email");
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+        })}
+        res.jeson({
+            success:true,user 
+        })
+        
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Server Error."
+        });
+    }
+ }
+
+ // to update user profile
+ export async function updsteProfile(req,res){
+    const {name.email} = req.body;
+    if(!email || !name || !validator.isEmail(email)){
+        return res.status(400).json({
+            success:false,
+            message:"Valid Email and Name are required."
+        })
+    }
+    try {
+        const exist = await User.findOne({email, _id:{$ne:req.user.id}});
+        if(exist){
+            return res.status(409).json({
+                success:false,
+                message:"Email already in use by another user."
+            })
+        }
+        const user = await User.findByIdAndUpdate(
+            req.user.id,
+            {name,email},
+            {new:true, runValidators:true,select:"name email"}
+        );
+
+        res.jeson({
+            success:true,
+            user
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Server Error."
+        });
+    }
  }
